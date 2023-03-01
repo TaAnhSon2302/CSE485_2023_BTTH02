927e3997fef6c22d2e4431b598ad67ac23710417
minh152Dlz <99807270+minh152Dlz@users.noreply.github.com>
2023-03-01 11:50:50 +0700

    pull commit article
    pull 1

--- btth02v2/models/Article.php
+++ btth02v2/models/Article.php
@@ -2,19 +2,79 @@
 class Article{
     // Thuộc tính
 
-    private $title;
-    private $summary;
-    private $cat_name;
+    private $tieude;
+    private $tomtat;
+    private $tenbhat;
+    private $matloai,$noidung,$matgia,$ngayviet,$hinhanh;
 
 
-    public function __construct($title, $summary,$cat_name){
-        $this->title = $title;
-        $this->summary = $summary;
-        $this->cat_name = $cat_name;
+    public function __construct($tieude,$tenbhat,$matloai ,$tomtat,$noidung,$matgia,$ngayviet,$hinhanh){
+        $this->tieude = $tieude;
+        $this->tenbhat = $tenbhat;
+
+        $this->matloai = $matloai;
+        $this->tomtat = $tomtat;
+        $this->noidung = $noidung;
+        $this->matgia = $matgia;
+        $this->ngayviet = $ngayviet;
+        $this->hinhanh = $hinhanh;
     }
 
     // Setter và Getter
-    public function getTitle(){
+    public function gettieude(){
         return $this->title;
     }
+    public function settieude($tieude){
+        $this->tieude = $tieude;
+    }
+
+    public function gettenbhat(){
+        return $this->tenbhat;
+    }
+    public function settenbhat($tenbhat){
+        $this->tenbhat = $tenbhat;
+    }
+
+    public function getmatloai(){
+        return $this->matloai;
+    }
+    public function setmatloai($matloai){
+        $this->matloai = $matloai;
+    }
+
+    public function gettomtat(){
+        return $this->tomtat;
+    }
+    public function settomtat($tomtat){
+        $this->tomtat = $tomtat;
+    }
+
+    public function getnoidung(){
+        return $this->noidung;
+    }
+    public function setnoidung($noidung){
+        $this->noidung = $noidung;
+    }
+
+    public function getmatgia(){
+        return $this->matgia;
+    }
+    public function setmatgia($matgia){
+        $this->matgia = $matgia;
+    }
+
+    public function gethinhanh(){
+        return $this->hinhanh;
+    }
+    public function sethinhanh($hinhanh){
+        $this->hinhanh = $hinhanh;
+    }
+
+    public function getngayviet(){
+        return $this->ngayviet;
+    }
+    public function setngayviet($ngayviet){
+        $this->ngayviet = $ngayviet;
+    }
+
 }
