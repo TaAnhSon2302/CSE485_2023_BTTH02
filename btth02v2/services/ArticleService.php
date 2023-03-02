abf9b9a9920615120461d2afc615980e7419ce1d
TaAnhSon2302 <99165276+TaAnhSon2302@users.noreply.github.com>
2023-03-02 11:34:29 +0700

    fix article
    fix artilce

--- btth02v2/services/ArticleService.php
+++ btth02v2/services/ArticleService.php
@@ -8,13 +8,15 @@ class ArticleService{
        $conn = $dbConn->getConnection();
 
         // B2. Truy vấn
-        $sql = "SELECT * FROM article INNER JOIN category ON article.category_id=category.id";
+        //$sql = "SELECT * FROM baiviet INNER JOIN theloai ON baiviet.ma_tloai=theloai.ma_tloai LEFT JOHN ";
+        $sql = "SELECT baiviet.*,theloai.ten_tloai, tacgia.ten_tgia FROM baiviet, theloai, tacgia WHERE baiviet.ma_tgia = tacgia.ma_tgia AND baiviet.ma_tloai = theloai.ma_tloai ORDER BY baiviet.ma_bviet ASC";
+
         $stmt = $conn->query($sql);
 
         // B3. Xử lý kết quả
         $articles = [];
         while($row = $stmt->fetch()){
-            $article = new Article($row['title'], $row['summary'], $row['name']);
+            $article = new article($row['tieude'], $row['ten_bhat'], $row['name'], $row['tieude'], $row['summary'], $row['name'], $row['tieude'], $row['summary']);
             array_push($articles,$article);
         }
         // Mảng (danh sách) các đối tượng Article Model
@@ -22,3 +24,4 @@ class ArticleService{
         return $articles;
     }
 }
+?>
