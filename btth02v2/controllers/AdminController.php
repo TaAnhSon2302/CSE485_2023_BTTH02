b9ee20489a59f948e606bd5a58ecd845bbdd4ba4
TaAnhSon2302 <99165276+TaAnhSon2302@users.noreply.github.com>
2023-03-04 17:16:40 +0700

    Admin index update Category function
    Admin index update Category function

--- btth02v2/controllers/AdminController.php
+++ btth02v2/controllers/AdminController.php
@@ -1,6 +1,8 @@
 <?php
   require_once("services/ArticleService.php");
   require_once("services/CategoryService.php");
+  require_once("services/AuthorService.php");
+  require_once("services/UserService.php");
   class AdminController{
     public function index(){
             $categoryService = new CategoryService();
@@ -8,6 +10,12 @@
 
             $articleService = new ArticleService();
             $article  = $articleService->getAllArticles();
+
+            $authorService = new AuthorService();
+            $author = $authorService->getAllAuthor();
+
+            $userService = new UserService();
+            $user = $userService->getAllUser();
             
         include('views/admin/index.php');
     }
